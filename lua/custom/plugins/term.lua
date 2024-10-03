bc11c4d9c0e88b48136b0ad0e20d74367c12a76d
4prndz <victorkouichi40@gmail.com>
2024-10-03 01:26:45 -0300

    :(

--- lua/custom/plugins/term.lua
+++ lua/custom/plugins/term.lua
@@ -3,11 +3,15 @@ return {
   config = function()
     require('toggleterm').setup {
       open_mapping = [[<c-\>]],
-      direction = 'float',
+      direction = 'horizontal',
+      size = 15,
       float_opts = {
         border = 'curved',
         width = 130,
         height = 20,
+      },
+      winbar = {
+        enabled = false,
       },
     }
   end,
